b1c0e5aa75bf5240c0d29d5558cacc5564aff7fa
karam-saadaoui <saadaoui.mohamed.karam@gmail.comgit config --global user.email saadaoui.mohamed.karam@gmail.comgit config --global user.email saadaoui.mohamed.karam@gmail.com>
2021-03-19 18:48:30 +0100

    almost finished

--- pairproject.js
+++ pairproject.js
@@ -1,28 +1,113 @@
-function makeStopwatch() {
-    return {
-        stop2: undefined,
-        time: 180,
-        startwatch: startwatch,
-        stopwatch: stopwatch,
-        reset: reset,
-        
-    };
+function Start () {
+    var minute = 1;
+    var sec = 30;
+    sta = setInterval(function() {
+      $("#timer").text(minute + " : " + sec);
+      sec--;
+      if (sec == 00) {
+        minute --;
+        sec = 59;
+        if (minute === 0 && sec===0) {
+          minute = 1;
+          sec = 30;
+         }
+      }
+    }, 1000);
+  }
+  function Stop() {
+    clearInterval(this.sta);
 }
 
-var startwatch = function() {
-    var that = this;
-    this.stop2 = setInterval(function() {
-        that.time = that.time -1;
-    },1000);
-};
+ 
+ 
+ function check(correct,answer){
+  for (var i=0;i<correct.length; i++){
+    
+      if(correct[i] !== answer[i]){
+        return false ;
+      }
+    }
+      return true;
+ 
+}
 
-var stopwatch = function() {
-    clearInterval(this.stop2);
-};
 
-var reset = function() {
-    clearInterval(this.stop2);
-    this.time = 0;
-};
+/*function clat (){
+   answer = [];
+  answer.push(document.getElementById('n1').value)
+  answer.push(document.getElementById('n2').value)
+  answer.push(document.getElementById('n3').value)
+  answer.push(document.getElementById('n4').value)
+  
+    if (check(correct,answer)){
+      stop() ;
+      alert("You GUESSED");
+    }
+    
+  }*/
+$(document).ready(function(){
+  var correct = [0,5,7,1];
+  $(".b1").click(function(){
+    Start();
+    $(".b2").click(function(){ 
+    answer=[];
+    answer.push(Number($('#n1').val()))
+    answer.push(Number($('#n2').val()))
+    answer.push(Number($('#n3').val()))
+    answer.push(Number($('#n4').val()))
+    if(check(correct,answer)===false){
+      stop();
+    $("#m2").hide();
+    $("#tac").show();
+    }
+    else{
+      stop();
+      alert("you guessed");
+    }console.log(check(correct,answer));
+    })
 
-////clearTimeout() 
+
+
+})
+})
+
+
+//   $(document).ready(function(){
+//     Start();
+//     stop() ; 
+//     clat() ;
+// })
+
+
+//  if (c1===C1 && c2===C2 && c3===C3 && c4===C4){
+  //       stop();
+  //       document.innerHTML('you guessed it sucker!');
+  //  }
+  //  else  if (c1!==C1 || c2!==C2 || c3!==C3 || c4!==C4){
+  //  $body.html('');
+  //  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
